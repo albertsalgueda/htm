7d79ca44d3de3487166bcaa1a072c22f6261afcf
Albert Salgueda <salguedainsti@gmail.com>
2022-03-25 19:14:30 -0500

    refactor

---
+++ htm/pool.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+
+#Compute the overlap with the current input for each column
+for c in columns:
+    overlap(c) = 0
+    for s in connectedSynapses(c):
+        overlap(c) = overlap(c) + input(t, s.sourceInput)
+        overlap(c) = overlap(c) * boost(c)
+
+#Compute the winning columns after inhibition
+for c in columns:
+    minLocalActivity = kthScore(neighbors(c), numActiveColumnsPerInhArea)
+    if overlap(c) > stimulusThreshold and overlap(c) >= minLocalActivity:
+        activeColumns(t).append(c)
+
+#Update synapse permanences and internal variables
+for c in activeColumns(t):
+    for s in potentialSynapses(c):
+        if active(s):
+            s.permanence += synPermActiveInc
+            s.permanence = min(1.0, s.permanence)
+        else:
+            s.permanence ­= synPermInactiveDec
+            s.permanence = max(0.0, s.permanence)
+for c in columns:
+    activeDutyCycle(c) = updateActiveDutyCycle(c)
+    activeDutyCycleNeighbors = mean(activeDutyCycle(neighbors(c))
+    boost(c) = boostFunction(activeDutyCycle(c), activeDutyCycleNeighbors)
+    overlapDutyCycle(c) = updateOverlapDutyCycle(c)
+    if overlapDutyCycle(c) < minDutyCycle(c):
+        increasePermanences(c, 0.1*connectedPerm)
+inhibitionRadius = averageReceptiveFieldSize()  
+              
--- visualization/test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-table = np.array(range(20))
-
